fd5260c8d5491ac4e4aa3945c49b69ba465f0138
rzusko <radoslav.zusko@gmail.com>
2021-08-27 17:12:26 +0200

    Added methods factorial and main to Factorial.java

--- src/BasicPrograms/Factorial.java
+++ src/BasicPrograms/Factorial.java
@@ -1,4 +1,48 @@
 package BasicPrograms;
 
+/*
+    Factorial of n is the product of all positive descending integers.
+    Factorial of n is denoted by n!. For example:
+        4! = 4*3*2*1 = 24
+        5! = 5*4*3*2*1 = 120
+
+    Here, 4! is pronounced as "4 factorial", it is also called "4 bang" or "4 shriek".
+    The factorial is normally used in Combinations and Permutations (mathematics).
+ */
+
+import java.util.Scanner;
+
 public class Factorial {
+
+    /**
+     * This method calculates factorial.
+     * @param n
+     * @return - factorial of entered number. If entered number is less than 1, then returns -1.
+     */
+    public static int factorial(int n) {
+        int factor;
+
+        if (n >= 1) {
+            factor = 1;
+
+            for (int i = 1; i <= n; i++) {
+                factor *= i;
+            }
+        } else {
+            factor = -1;
+        }
+
+        return factor;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number to calculate factorial: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        System.out.println(n + "! = " + factorial(n));
+    }
+
 }
