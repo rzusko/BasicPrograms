70a9e1ca46ed3e9b14ffac559e0eefd79987ea00
rzusko <radoslav.zusko@gmail.com>
2021-09-13 15:56:44 +0200

    Added class Patterns.java with methods: - rightTriangleStarPattern - mirroredRightTriangleStarPattern - leftTriangleStarPattern - mirroredLeftTriangleStarPattern - pyramidStarPattern

---
+++ src/BasicPrograms/Patterns.java
@@ -0,0 +1,72 @@
+package BasicPrograms;
+
+public class Patterns {
+
+    public static void rightTriangleStarPattern(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void mirroredRightTriangleStarPattern(int size) {
+        for (int i = size; i >= 1; i--) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void leftTriangleStarPattern(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = size; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void mirroredLeftTriangleStarPattern(int size) {
+        for (int i = size; i >= 1; i--) {
+            for (int j = size; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pyramidStarPattern(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void diamondShapePattern(int size) {
+
+    }
+
+    public static void main(String[] args) {
+//        rightTriangleStarPattern(5);
+//        mirroredRightTriangleStarPattern(5);
+//        leftTriangleStarPattern(5);
+//        mirroredLeftTriangleStarPattern(5);
+        pyramidStarPattern(6);
+    }
+
+}
