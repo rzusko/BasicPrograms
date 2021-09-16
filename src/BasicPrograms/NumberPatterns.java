4f50d70c8945cfdcfa8f1b0790758d406f941caa
rzusko <radoslav.zusko@gmail.com>
2021-09-16 18:15:54 +0200

    Added method to class NumberPatterns.java: - pattern1

--- src/BasicPrograms/NumberPatterns.java
+++ src/BasicPrograms/NumberPatterns.java
@@ -1,4 +1,16 @@
 package BasicPrograms;
 
 public class NumberPatterns {
+    public static void pattern1(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        pattern1(7);
+    }
 }
