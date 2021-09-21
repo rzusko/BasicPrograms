0dbc877209eb6c5a89905a909bf80e151bb3db47
rzusko <radoslav.zusko@gmail.com>
2021-09-21 18:50:45 +0200

    Methods added to class CharacterPatterns.java: - kShapeAlphabetPattern

--- src/BasicPrograms/CharacterPatterns.java
+++ src/BasicPrograms/CharacterPatterns.java
@@ -1,6 +1,7 @@
 package BasicPrograms;
 
 public class CharacterPatterns {
+    private final static int ALPHA = 65;
 
     /*
         Output for size = 5:
@@ -14,7 +15,7 @@ public class CharacterPatterns {
     public static void rightTriangleAlphabeticPattern(int size) {
         for (int i = 1; i <= size; i++) {
             for (int j = 0; j < i; j++) {
-                System.out.print((char) (j + 65) + " ");
+                System.out.print((char) (j + ALPHA) + " ");
             }
             System.out.println();
         }
@@ -32,7 +33,36 @@ public class CharacterPatterns {
     public static void repeatingAlphabetPattern(int size) {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j <= i; j++) {
-                System.out.print((char) (i + 65) + " ");
+                System.out.print((char) (i + ALPHA) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    /*
+        Output for size = 5:
+
+        A B C D E
+        A B C D
+        A B C
+        A B
+        A
+        A B
+        A B C
+        A B C D
+        A B C D E
+
+     */
+    public static void kShapeAlphabetPattern(int size) {
+        for (int i = size; i > 1; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print((char) (j + ALPHA) + " ");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= size; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print((char) (j + ALPHA) + " ");
             }
             System.out.println();
         }
@@ -40,6 +70,8 @@ public class CharacterPatterns {
 
     public static void main(String[] args) {
 //        rightTriangleAlphabeticPattern(5);
-        repeatingAlphabetPattern(5);
+//        repeatingAlphabetPattern(5);
+        kShapeAlphabetPattern(5);
     }
+
 }
