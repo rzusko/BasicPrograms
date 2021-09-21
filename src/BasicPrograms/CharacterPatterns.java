e56e9e4f9d40758c4acadd6b20a8e02de9361008
rzusko <radoslav.zusko@gmail.com>
2021-09-21 18:36:26 +0200

    Methods added to class CharacterPatterns.java: - rightTriangleAlphabeticPattern

--- src/BasicPrograms/CharacterPatterns.java
+++ src/BasicPrograms/CharacterPatterns.java
@@ -1,4 +1,26 @@
 package BasicPrograms;
 
 public class CharacterPatterns {
+
+    /*
+        Output for size = 5:
+
+        A
+        A B
+        A B C
+        A B C D
+        A B C D E
+     */
+    public static void rightTriangleAlphabeticPattern(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print((char) (j + 65) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        rightTriangleAlphabeticPattern(5);
+    }
 }
